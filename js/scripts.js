04fe727d2543c974d9f1b408cd76da84cb12482a
elisfreire1 <elisfreire1@gmail.com>
2025-01-09 08:36:20 +0100

    Lógica do Quiz: Perguntas

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,69 @@ const letters = ['a', 'b', 'c', 'd']; // alternativas da questão
 let points = 0; // pontos do usuário: zero pq vai progredir conforme o usuário acerta as questões
 let actualQuestion = 0; // questão atual: zero pq vai progredir conforme o usuário responde as questões
 
+// Perguntas do quizz
+const questions = [
+    {
+        "question": "PHP foi desenvolvido para qual fim?",
+        "answers": [
+            {
+                "answer": "back-end",
+                "correct": true
+            },
+            {
+                "answer": "front-end",
+                "correct": false
+            },
+            {
+                "answer": "Sistema operacional",
+                "correct": false
+            },
+            {
+                "answer": "Banco de dados",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Uma forma de declarar variável em JavaScript:",
+        "answers": [
+            {
+                "answer": "$var",
+                "correct": false
+            },
+            {
+                "answer": "var",
+                "correct": true
+            },
+            {
+                "answer": "@var",
+                "correct": false
+            },
+            {
+                "answer": "#let",
+                "correct": false
+            }
+        ]
+    },
+    {
+        "question": "Qual o seletor de id no CSS?",
+        "answers": [
+            {
+                "answer": "#",
+                "correct": true
+            },
+            {
+                "answer": ".",
+                "correct": false
+            },
+            {
+                "answer": "@",
+                "correct": false
+            },
+            {
+                "answer": "/",
+                "correct": false
+            }
+        ]
+    }
+]
