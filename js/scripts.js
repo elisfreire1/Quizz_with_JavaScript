6607fbcc1b008974e049392bf91d5c8eb94c7762
elisfreire1 <elisfreire1@gmail.com>
2025-01-09 21:12:09 +0100

    Lógica do Quiz: iniciar o quizz com init()

--- js/scripts.js
+++ js/scripts.js
@@ -3,12 +3,14 @@ const question = document.getElementById('question'); // div que contém a pergu
 const answersBox = document.querySelector('#answers-box'); // div que contém as respostas alternativas
 const quizzContainer = document.querySelector('#quizz-container'); // div que contém o quizz
 const scoreContainer = document.getElementById('score-container'); // div que contém a tela de score
+
 const letters = ['a', 'b', 'c', 'd']; // alternativas da questão
 let points = 0; // pontos do usuário: zero pq vai progredir conforme o usuário acerta as questões
 let actualQuestion = 0; // questão atual: zero pq vai progredir conforme o usuário responde as questões
 
 // Perguntas do quizz
 const questions = [
+    // questão 1
     {
         "question": "PHP foi desenvolvido para qual fim?",
         "answers": [
@@ -30,6 +32,7 @@ const questions = [
             }
         ]
     },
+    // questão 2
     {
         "question": "Uma forma de declarar variável em JavaScript:",
         "answers": [
@@ -51,6 +54,7 @@ const questions = [
             }
         ]
     },
+    // questão 3
     {
         "question": "Qual o seletor de id no CSS?",
         "answers": [
@@ -72,4 +76,29 @@ const questions = [
             }
         ]
     }
-]
+];
+
+// função para iniciar o quizz: substituir a tela inicial pela tela inicial do quizz, ou seja, a 1ª pergunta
+function init() {
+   // criar a primeira pergunta
+    createQuestion(0); // 0 refere-se à primeira questão, no caso a questão 1 e assim por diante
+}
+
+// função para criar e inserir as alternativas na tela
+function createQuestion(i){ // i refere-se à questão atual
+    //limpar a questão anterior
+    const oldButtons = answersBox.querySelectorAll('button'); // pegar todos os botões da questão anterior, no caso, as letras a, b, c, d.
+    oldButtons.forEach(function(btn){
+        btn.remove(); // remover cada botão
+    });
+    // alterar o texto da pergunta
+    const questionText = question.querySelector("#question-text"); // pegar a questão atual
+    const questionNumber = question.querySelector("#question-number"); // pegar o número da questão atual
+
+    // alterar o texto da pergunta com array de questões
+    questionText.textContent = questions[i].question; // pegar a questão atual representada pelo índice i
+    questionNumber.textContent = i + 1; // pegar o número da questão atual, actualQuestion, representada pelo índice i
+}
+
+// inicializar o quizz
+init(); // chamar a função init para iniciar o quizz
