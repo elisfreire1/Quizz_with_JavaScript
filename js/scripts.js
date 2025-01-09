2e3970fd190988b1c1c755d4aa06b2399cb9c112
elisfreire1 <elisfreire1@gmail.com>
2025-01-09 08:35:12 +0100

    Lógica do Quiz: declaração de variáveis

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("External JS file");
+// Declaração de variáveis
+const question = document.getElementById('question'); // div que contém a pergunta
+const answersBox = document.querySelector('#answers-box'); // div que contém as respostas alternativas
+const quizzContainer = document.querySelector('#quizz-container'); // div que contém o quizz
+const scoreContainer = document.getElementById('score-container'); // div que contém a tela de score
+const letters = ['a', 'b', 'c', 'd']; // alternativas da questão
+let points = 0; // pontos do usuário: zero pq vai progredir conforme o usuário acerta as questões
+let actualQuestion = 0; // questão atual: zero pq vai progredir conforme o usuário responde as questões
+
